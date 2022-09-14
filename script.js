0537e591623d89fdef6b56fa185fa9aeb81c6a36
Oscar Martin Mora <moramartin99@gmail.com>
2022-09-14 15:15:14 -0600

    Crear funciones para agregar y remover divs

--- script.js
+++ script.js
@@ -8,3 +8,17 @@ let setGridTemplates = (element, base) => {
     element.style.gridTemplate = `repeat(${base}, 1fr) / repeat(${base}, 1fr)`;
 };
 
+let addDivChildren = (parent, nChild, classList) => {
+    for (i = 1; i <= nChild; i++) {
+        let child = document.createElement("div");
+        child.classList.add(classList);
+        parent.append(child);
+    }
+};
+
+let removeAllChildren = (parent) => {
+    let childrenArr = Array.from(parent.querySelectorAll("*"));
+    childrenArr.forEach((child) => {
+        child.remove();
+    });
+};
