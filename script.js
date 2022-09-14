8e788b9361466a8d48761923c71cbdb8235126be
Oscar Martin Mora <moramartin99@gmail.com>
2022-09-14 15:14:06 -0600

    Crear setGridTemplates()

--- script.js
+++ script.js
@@ -3,3 +3,8 @@
 let canvas = document.querySelector("#canvas");
 let gridBase;
 /* --------------------------------------------------------------------------------------------------- */
+
+let setGridTemplates = (element, base) => {
+    element.style.gridTemplate = `repeat(${base}, 1fr) / repeat(${base}, 1fr)`;
+};
+
